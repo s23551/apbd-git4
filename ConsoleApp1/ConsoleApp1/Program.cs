7445608d8906e9b644714781a90d95ce8f0e4c78
s23551 <s23551@pjwstk.edu.pl>
2024-03-20 23:58:14 +0100

    Prepared tests

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,30 @@ Console.WriteLine("Test2");
 
 Console.WriteLine("Test3");
 
-for (int i = 10; i > 1; i--)
+static double Average(int[] array)
 {
-    Console.WriteLine(i);
+    if (array == null || array.Length < 1)
+    {
+        throw new ArgumentException("Input array incorrect");
+    }
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+
+    return sum/array.Length;
+}
+
+static void test()
+{
+    int[] ints = { 1, 2, 3, 4, 5, };
+    int[] ints1 = { 1, 2 };
+    int[] ints2 = { 3 };
+    int[] ints3 = { 4,5 };
+
+    Console.WriteLine("Test 1 = " + (Average(ints) == 3));
+    Console.WriteLine("Test 2 = " + (Average(ints1) == 1.5));
+    Console.WriteLine("Test 3 = " + (Average(ints2) == 3));
+    Console.WriteLine("Test 4 = " + (Average(ints3) == 4.5));
 }
